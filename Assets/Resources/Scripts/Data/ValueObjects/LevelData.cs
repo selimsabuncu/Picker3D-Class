ebf351a67a57d5c5cabc5e7df3ab7685522e8fd5
Mengart0 <sesabuncu@hotmail.com>
2022-10-31 19:38:17 +0300

    First Commit
    25/10/22

---
+++ Assets/Resources/Scripts/Data/ValueObjects/LevelData.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using System;
+
+[Serializable]
+public class LevelData
+{
+    public List<PoolData> PoolList = new List<PoolData>();
+}
+
+
+[Serializable]
+public struct PoolData
+{
+    public sbyte RequiredObjectCount;
+}
