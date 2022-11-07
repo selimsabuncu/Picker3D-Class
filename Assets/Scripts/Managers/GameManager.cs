9eced1a4657aceddccabbe618c87cc007f48c7aa
Mengart0 <sesabuncu@hotmail.com>
2022-11-07 22:20:42 +0300

    Added Packages
    -added packages (GUI Hyper Casual, DOTween, ODIN)
    -added GameManager script

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using Enums;
+using Sirenix.OdinInspector;
+
+public class GameManager : MonoBehaviour
+{
+    #region Self Variables
+
+    #region Serialized Variables
+
+    [SerializeField] private GameStates states;
+
+    #endregion
+
+    #endregion
+
+
+    private void Awake()
+    {
+        Application.targetFrameRate = 60;
+    }
+
+    private void OnEnable()
+    {
+        SubscribeEvents();
+    }
+
+    private void SubscribeEvents()
+    {
+        CoreGameSignals.Instance.onChangeGameState += OnChangeGameState;
+    }
+
+    private void UnSubscribeEvents()
+    {
+        CoreGameSignals.Instance.onChangeGameState -= OnChangeGameState;
+    }
+
+    private void OnDisable()
+    {
+        UnSubscribeEvents();
+    }
+
+    [Button("Change State")]
+    private void OnChangeGameState(GameStates state)
+    {
+        states = state;
+    }
+
+}
--- Assets/Scripts/Signals/CoreGameSignals.cs
+++ Assets/Scripts/Signals/CoreGameSignals.cs
@@ -1,5 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
+using Enums;
 using UnityEngine;
 using UnityEngine.Events;
 
